49cb4c8fa866ad58037acf875df5dca99d3f3376
mir97 <78717265+mir97@users.noreply.github.com>
2021-02-09 14:37:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array==undefined || array.length==0){ return 0;}
+    var min = array[0];
+    for(i=0;i<array.length;i++){
+        if(array[i]<min) {
+            min = array[i];
+        }
+    }
+    return  min;
 }
 
+
+console.log(exports.min([2,-24,13,-11,16,-12,-16,24,13,-38,19,-16,19,3,2,32,0,-10,-19,-27]));
+
 exports.max = function max (array) {
-  return 0;
+    if(array==undefined || array.length==0){ return 0;}
+
+    var max = array[0];
+    for(i=0;i<array.length;i++){
+        if(array[i]>max) {
+            max = array[i];
+        }
+    }
+  return  max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array==undefined || array.length==0){ return 0;}
+    var sum=0;
+    for(i=0;i<array.length;i++){
+        sum=sum+array[i];
+    }
+    return sum/array.length;
 }
